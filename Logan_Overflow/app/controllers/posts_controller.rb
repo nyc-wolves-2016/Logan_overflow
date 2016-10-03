1f2500de66eda2143333ee8a8ed66dfb7153cb01
Thomas Graves <email": null>
2016-10-03 15:24:31 -0400

    Add in CR functionality for posts.

--- Logan_Overflow/app/controllers/posts_controller.rb
+++ Logan_Overflow/app/controllers/posts_controller.rb
@@ -13,5 +13,19 @@ class PostsController < ApplicationController
   def edit
     @post = Post.find(params[:id])
   end
-  
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
 end
