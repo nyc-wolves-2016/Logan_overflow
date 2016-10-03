7bea89fe9631d309019824097a0385aea78dd1de
Thomas Graves <email": null>
2016-10-03 15:59:09 -0400

    Add in CRUD functionality for sessions controller.

--- Logan_Overflow/app/controllers/users_controller.rb
+++ Logan_Overflow/app/controllers/users_controller.rb
@@ -1,14 +1,2 @@
 class UsersController < ApplicationController
-
-  def profile
-    @user = User.find(params[:id])
-  end
-
-  def new
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-  
 end
