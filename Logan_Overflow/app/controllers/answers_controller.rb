cd2ca866921a6d24fc6b400f4a35866a4269c953
Thomas Graves <email": null>
2016-10-03 17:43:20 -0400

    Give answers create and destroy functionality.

--- Logan_Overflow/app/controllers/answers_controller.rb
+++ Logan_Overflow/app/controllers/answers_controller.rb
@@ -1,12 +1,27 @@
 class AnswersController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @answer = @post.answers.create(answer_params)
+    @answer = @post.answers.new(answer_params)
+    @answer.post_id = params[:post_id]
+    @answer.user_id = current_user.id
+
+    if @answer.save
+      redirect_to post_path(@post)
+    else
+      render post_path(@post)
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @answer = @post.answers.find(params[:id])
+    @answer.destroy
 
     redirect_to post_path(@post)
   end
 
-  def destroy
-    @post = Post.find
-  end
+  private
+    def answer_params
+      params.require(:answer).permit(:content)
+    end
 end
