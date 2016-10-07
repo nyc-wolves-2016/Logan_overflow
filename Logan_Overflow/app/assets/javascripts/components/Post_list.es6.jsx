9cdebb81ab046a1e2007839bc0a79882b6c1c06f
Angel Chen & Thomas Graves <pair+angel-chen.thomaslogangraves@devbootcamp.com>
2016-10-07 15:21:52 -0400

    Start react.

--- Logan_Overflow/app/assets/javascripts/components/Post_list.es6.jsx
+++ Logan_Overflow/app/assets/javascripts/components/Post_list.es6.jsx
@@ -1,7 +1,7 @@
 class PostList extends React.Component {
   render() {
     return (
-      Hello
+
     )
   }
 }
--- Logan_Overflow/app/controllers/posts_controller.rb
+++ Logan_Overflow/app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    Post.all.ordered_json
   end
 
   def show
